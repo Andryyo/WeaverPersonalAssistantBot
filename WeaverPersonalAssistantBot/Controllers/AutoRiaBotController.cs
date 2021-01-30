7bd820ab907e7e5d5e3cda64a43bb77e6931b8c1
Andrii <andryyo@ukr.net>
2021-01-31 00:06:01 +0200

    Initial submit

---
+++ WeaverPersonalAssistantBot/Controllers/AutoRiaBotController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeaverPersonalAssistantBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoRiaBotController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Webhook()
+        {
+            return Ok("Hello");
+        }
+    }
+}
